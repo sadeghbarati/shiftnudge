5c5a0d0b69311e65ce058e2f2936c0025f3c66d0
Matt D. Smith <mds@mds.lan>
2019-11-13 14:06:19 -0500

    added show hide toggle for lessons

--- src/_assets/js/shiftnudge.js
+++ src/_assets/js/shiftnudge.js
@@ -3,19 +3,27 @@
 
 $(document).ready(function() {
 
-  console.log("Thanks for popping open the inspector.\nIf you see something wrong, please let me know!\nA DM or public reply is perfectly acceptable.\n\n—MDS\nhttp://twitter.com/mds\n\n");
+	console.log("Thanks for popping open the inspector.\nIf you see something wrong, please let me know!\nA DM or public reply is perfectly acceptable.\n\n—MDS\nhttp://twitter.com/mds\n\n");
 
-  $('.sn-countdown').countdown('2019/12/13', function(event) {
-    $(this).html(event.strftime('%Dd %Hh %Mm %Ss'));
-  });
+	// countdown
+	$('.sn-countdown').countdown('2019/12/13', function(event) {
+		$(this).html(event.strftime('%Dd %Hh %Mm %Ss'));
+	});
 
-  window.addEventListener("load", () => {
-    document.querySelector("body").classList.add("loaded"); 
-   });
+	// make sure things are loaded
+	window.addEventListener("load", () => {
+		document.querySelector("body").classList.add("loaded"); 
+	 });
 
-  var now = new Date();
-  var year = now.getFullYear();
-  $('.copyright').html('©'+year);
+	// show hide full lesson list
+	$('.sn-lessons').on( "click", function(event) {
+		$('.sn-lessons').toggleClass( "sn-lessons--showing" );
+	});
+
+	// dynamic year in footer
+	var now = new Date();
+	var year = now.getFullYear();
+	$('.copyright').html('©'+year);
 
 });
 
