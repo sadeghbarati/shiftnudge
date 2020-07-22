b6958b6562364484232222b80ac2bdd1f9ef308f
Matt D. Smith <mds@mds-16.lan>
2020-07-22 09:55:05 -0400

    added reviews page

--- src/_assets/js/shiftnudge.js
+++ src/_assets/js/shiftnudge.js
@@ -1,12 +1,46 @@
+// @codekit-prepend 'vendor/imagesloaded.min.js'
 // @codekit-prepend 'vendor/jquery.js'
 // @codekit-prepend 'vendor/jquery.countdown.min.js'
 
+
+// grid for review page
+function resizeGridItem(item){
+	grid = document.getElementsByClassName("grid")[0];
+	rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+	rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+	rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+	item.style.gridRowEnd = "span "+rowSpan;
+}
+
+function resizeAllGridItems(){
+	allItems = document.getElementsByClassName("item");
+	for(x=0;x<allItems.length;x++){
+	  resizeGridItem(allItems[x]);
+	}
+}
+
+window.onload = resizeAllGridItems();
+
+window.addEventListener("resize", resizeAllGridItems);
+
+allItems = document.getElementsByClassName("item");
+	for(x=0;x<allItems.length;x++){
+	imagesLoaded( allItems[x], resizeInstance);
+}
+
+function resizeInstance(instance){
+	item = instance.elements[0];
+	resizeGridItem(item);
+}
+
+
+// jquery stuff
 $(document).ready(function() {
 
-	console.log("Thanks for popping open the inspector.\nIf you see something wrong, please let me know!\nA DM or public reply is perfectly acceptable.\n\n—MDS\nhttp://twitter.com/mds\n\n");
+	console.log("Thanks for popping open the inspector.\nI hand-coded everything myself so if \nyou see something wrong, let me know!\n\nA DM or public reply is perfectly acceptable. 👍\n\n—MDS\nhttp://twitter.com/mds\n\n");
 
 	// countdown
-	$('.sn-countdown').countdown('2019/12/21', function(event) {
+	$('.sn-countdown').countdown('2020/04/9', function(event) {
 		$(this).html(event.strftime('%Dd %Hh %Mm %Ss'));
 	});
 
@@ -21,7 +55,7 @@ $(document).ready(function() {
 	});
 
 	$(".formkit-field input").on('focus blur', function(){
-  	$(this).parent().parent().toggleClass('is_focused');
+		$(this).parent().parent().toggleClass('is_focused');
 	})
 
 	// dynamic year in footer
@@ -29,5 +63,13 @@ $(document).ready(function() {
 	var year = now.getFullYear();
 	$('.copyright').html('©'+year);
 
-});
+	// randomize order of testimonials
+	var cards = $(".sn-student-quote");
+	for(var i = 0; i < cards.length; i++){
+	    var target = Math.floor(Math.random() * cards.length -1) + 1;
+	    var target2 = Math.floor(Math.random() * cards.length -1) +1;
+	    cards.eq(target).before(cards.eq(target2));
+	}
 
+
+});
