a202633b497dfb76fbb45a72a27b495a99843aa2
Matt D. Smith <mds@mds-16.attlocal.net>
2020-08-05 16:47:12 -0400

    reorganized modules, added hero section, and other style and content updates

--- src/_assets/js/shiftnudge.js
+++ src/_assets/js/shiftnudge.js
@@ -71,5 +71,31 @@ $(document).ready(function() {
 	    cards.eq(target).before(cards.eq(target2));
 	}
 
+	// anime.js stuff
+	// Wrap every letter in a span
+	
+	// var textWrapper = document.querySelector('.ml7 .letters');
+	// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+	// anime.timeline({loop: true})
+	//   .add({
+	//     targets: '.ml7 .letter',
+	//     translateY: ["1.1em", 0],
+	//     translateX: ["0.55em", 0],
+	//     translateZ: 0,
+	//     rotateZ: [180, 0],
+	//     duration: 750,
+	//     easing: "easeOutExpo",
+	//     delay: (el, i) => 50 * i
+	//   }).add({
+	//     targets: '.ml7',
+	//     opacity: 0,
+	//     duration: 1000,
+	//     easing: "easeOutExpo",
+	//     delay: 1000
+	//   });
+
 
 });
+
+
