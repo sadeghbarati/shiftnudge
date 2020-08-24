f0ccb47df4d9e0f6145f57a4c425c38f5b53fa88
Matt D. Smith <mds@mds.lan>
2020-08-23 22:05:56 -0400

    fixed footer and back button on review page

--- src/_assets/js/shiftnudge.js
+++ src/_assets/js/shiftnudge.js
@@ -68,41 +68,6 @@ $(document).ready(function() {
 	    $(this).css('cursor', '');
 	});
 
-
-	// $(".formkit-field input").on('focus blur', function(){
-	// 	$(this).parent().parent().toggleClass('is_focused');
-	// })
-
-	// grid for review page
-	// function resizeGridItem(item){
-	// 	grid = document.getElementsByClassName("grid")[0];
-	// 	rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
-	// 	rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-	// 	rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
-	// 	item.style.gridRowEnd = "span "+rowSpan;
-	// }
-
-	// function resizeAllGridItems(){
-	// 	allItems = document.getElementsByClassName("item");
-	// 	for(x=0;x<allItems.length;x++){
-	// 	  resizeGridItem(allItems[x]);
-	// 	}
-	// }
-
-	// window.onload = resizeAllGridItems();
-
-	// window.addEventListener("resize", resizeAllGridItems);
-
-	// allItems = document.getElementsByClassName("item");
-	// 	for(x=0;x<allItems.length;x++){
-	// 	imagesLoaded( allItems[x], resizeInstance);
-	// }
-
-	// function resizeInstance(instance){
-	// 	item = instance.elements[0];
-	// 	resizeGridItem(item);
-	// }
-
 	// dynamic year in footer
 	var now = new Date();
 	var year = now.getFullYear();
@@ -123,15 +88,17 @@ $(document).ready(function() {
 		$('.payment-option-one-time').toggleClass( "is-selected" );
 
 		if ($( ".payment-option-monthly" ).hasClass( "is-selected" )) {
+
 			$(".sn-core .sn-cta").attr("href", "https://transactions.sendowl.com/subscriptions/15428/AB8F3CAF/purchase")
 			$(".sn-pro .sn-cta").attr("href", "https://transactions.sendowl.com/subscriptions/15427/526D9F28/purchase")
+		
 		} else {
+
 			$(".sn-core .sn-cta").attr("href", "https://transactions.sendowl.com/products/78343146/21A4DE94/purchase")
 			$(".sn-pro .sn-cta").attr("href", "https://transactions.sendowl.com/products/78343087/E3E6D25A/purchase")
 		}
 		
 	});
-
 
 });
 
